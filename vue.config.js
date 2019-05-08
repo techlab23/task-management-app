ffe36285fac47050b366010bd69b19a5710074fa
Shirish Nigam <nigamshirish@gmail.com>
2019-05-08 18:13:00 +1000

    cleaned vue.config.js and renamed npm command from deploy-dev to deploy-staging and updated deployment url as well.

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  publicPath: undefined,
-  outputDir: undefined,
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: false,
-  parallel: undefined,
-  css: undefined
+  productionSourceMap: false
 }
